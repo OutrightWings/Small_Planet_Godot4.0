f71fdc6ff03208ad7478b536ced4747c9b808bcd
lilym <lilymason@msn.com>
2024-06-26 14:17:11 -0700

    Gitignore

--- Scripts/GlobalData.cs
+++ Scripts/GlobalData.cs
@@ -1,6 +1,7 @@
 using Godot;
 
 public partial class GlobalData : Node{
+    #region Global Vars
     public static int DIMENSIONS = 250;
     public static float HEIGHT = 100;
     public static float FLOOR = -5;
@@ -10,34 +11,40 @@ public partial class GlobalData : Node{
     public static float MAX_WOOD = 10;
     public static float BASE_PLANT_GROWTH_RATE = 0.1f;
     public static int REGION_SELECTION_RADIOUS = 2;
-    
-    public Terrain water_mesh, ground_mesh;
-
+    #endregion
+    #region Signals
     [Signal]
     public delegate void ReloadEventHandler();
     [Signal]
     public delegate void ClockEventHandler();
+    #endregion
+    #region Ready
+    public Terrain water_mesh, ground_mesh;
     public override void _Ready()
     {
        water_mesh = (Terrain)GetNode("Water/WaterMesh");
         ground_mesh = (Terrain)GetNode("Ground/GroundMesh");
         //GenerateMeshes();
     }
+    private void GenerateMeshes(){
+        ground_mesh.GenerateTerrain();
+        water_mesh.GenerateTerrain();
+        EmitSignal("Clock");
+    }
+    #endregion
     public override void _Process(double delta)
     {
         if(Input.IsActionJustPressed("Reload")){
            EmitSignal("Reload");
         }
     }
-    private void GenerateMeshes(){
-        ground_mesh.GenerateTerrain();
-        water_mesh.GenerateTerrain();
-        EmitSignal("Clock");
-    }
+    
+    #region Util
     public static int Index(int row, int col){
         return Index(row,col,DIMENSIONS);
     }
     public static int Index(int row, int col, int dim){
         return (row * dim) + col;
     }
+    #endregion
 }
